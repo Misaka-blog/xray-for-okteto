88c41a81a539496a9fc819557b5f0bb3bda203f1
Muhammedslp <130940620+Muhammedslp@users.noreply.github.com>
2023-04-17 03:52:08 +0330

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
-VMESS_WSPATH='/vmess'
-VLESS_WSPATH='/vless'
-TROJAN_WSPATH='/trojan'
-SS_WSPATH='/shadowsocks'
+UUID='9f9bf634-4109-4f8b-845f-21db1f5e819a'
 NEZHA_SERVER=''
 NEZHA_PORT=''
 NEZHA_KEY=''
